d2e60cd9f00799dee0f4ef0fed7061f6267f9318
Nolan MacDonald <66697853+nolmacdonald@users.noreply.github.com>
2024-11-11 19:18:58 -0500

    class DataWrangler pulls all email data from json files

--- src/data_wrangler.py
+++ src/data_wrangler.py
@@ -14,29 +14,35 @@ class DataWrangler:
 
     def parse_emails(self, save_csv_path=None, save_db_path=None, table_name="emails"):
         """
-        Load emails from JSON files located in the specified directory and return them as a pandas DataFrame.
+        Load emails from JSON files located in the specified directory and return them
+        as a pandas DataFrame.
 
-        This method reads all JSON files in the directory specified by `self.json_dir`, extracts relevant email
-        information from each file, and compiles the data into a pandas DataFrame. The method also logs the
-        progress and time taken to process each file and the overall operation.
+        This method reads all JSON files in the directory specified by `self.json_dir`,
+        extracts relevant email information from each file, and compiles the data into
+        a pandas DataFrame. The method also logs the progress and time taken to process
+        each file and the overall operation.
 
         Returns:
-            pd.DataFrame: A DataFrame containing the loaded email data with the following columns:
+            pd.DataFrame: A DataFrame containing email data with the following columns:
+            - text: The body text of the email.
             - message_id: The unique identifier of the email.
             - date: The date the email was sent.
             - from: The email address of the sender.
-            - from_name: The name of the sender.
             - to: The email addresses of the recipients.
-            - to_name: The names of the recipients.
-            - cc: The email addresses of the CC recipients.
-            - cc_name: The names of the CC recipients.
-            - bcc: The email addresses of the BCC recipients.
-            - bcc_name: The names of the BCC recipients.
             - subject: The subject of the email.
-            - text: The body text of the email.
+            - cc: The email addresses of the CC recipients.
+            - bcc: The email addresses of the BCC recipients.
+            - mime-version: The MIME version of the email.
+            - content-type: The content type of the email.
+            - content-transfer-encoding: The content transfer encoding of the email.
+            - x-from: The name of the sender.
+            - x-to: Names of the recipients.
+            - x-cc: Names of the CC recipients.
+            - x-bcc: Names of the BCC recipients.
             - folder: The folder where the email is stored.
             - origin: The origin of the email.
             - filename: The filename of the email.
+            - priority: The priority of the email.
 
         Raises:
             FileNotFoundError: If the specified directory does not exist.
@@ -65,29 +71,49 @@ class DataWrangler:
                     data = json.load(file)  # Load the JSON data
                     # Extract relevant fields from the JSON data
                     email_data = {
+                        # Main email data
+                        "text": data.get("text", ""),
+                        # Headers
                         "message_id": data["headers"].get("message-id", ""),
                         "date": data["headers"].get("date", ""),
                         "from": data["headers"].get("from", ""),
-                        "from_name": data["headers"].get("x-from", ""),
                         "to": data["headers"].get("to", ""),
-                        "to_name": data["headers"].get("x-to", ""),
-                        "cc": data["headers"].get("cc", ""),
-                        "cc_name": data["headers"].get("x-cc", ""),
-                        "bcc": data["headers"].get("bcc", ""),
-                        "bcc_name": data["headers"].get("x-bcc", ""),
                         "subject": data["headers"].get("subject", ""),
-                        "text": data.get("text", ""),
+                        "cc": data["headers"].get("cc", ""),
+                        "bcc": data["headers"].get("bcc", ""),
+                        "mime-version": data["headers"].get("mime-version", ""),
+                        "content-type": data["headers"].get("content-type", ""),
+                        "content-transfer-encoding": data["headers"].get("", ""),
+                        "x-from": data["headers"].get("x-from", ""),
+                        "x-to": data["headers"].get("x-to", ""),
+                        "x-cc": data["headers"].get("x-cc", ""),
+                        "x-bcc": data["headers"].get("x-bcc", ""),
                         "folder": data["headers"].get("x-folder", ""),
                         "origin": data["headers"].get("x-origin", ""),
                         "filename": data["headers"].get("x-filename", ""),
+                        # Main email data
+                        # Commented out since these are duplicate information
+                        # "subject": data.get("subject", ""),
+                        # "messageId": data.get("messageId", ""),
+                        # "date": data.get("date", ""),
+                        # "from": data.get("from", ""),
+                        # "to": data.get("to", ""),
+                        # "cc": data.get("cc", ""),
+                        # "bcc": data.get("bcc", ""),
+                        # "date": data.get("date", ""),
+                        "priority": data.get("priority", ""),
                     }
                     data_list.append(email_data)  # Add the email data to the list
 
                 file_end_time = (
                     time.time()
                 )  # Record the end time of processing the file
+
+                # Log
+                # Total time to parse emails
+                log_proc = f"{file_end_time - file_start_time:.6f} s"
                 self.logger.info(
-                    f"Finished processing file: {file_path} in {file_end_time - file_start_time:.6f} seconds"
+                    f"Finished processing file: {file_path} in {log_proc}"
                 )  # Log the time taken to process the file
 
         # Convert the list of email data to a DataFrame
@@ -102,15 +128,16 @@ class DataWrangler:
         )
 
         # Save the DataFrame to a CSV file if requested
-
         if save_csv_path:
             manager = DatabaseManager(file_path=save_csv_path)
             manager.save_to_csv(emails_df)
+            self.logger.info(f"Parsed dataset saved to CSV file: {save_csv_path}")
 
         # Save the DataFrame to a SQLite database if requested
         if save_db_path:
             manager = DatabaseManager(file_path=save_db_path)
             manager.save_to_db(emails_df, table_name)
+            self.logger.info(f"Parsed dataset saved to SQLite database: {save_db_path}")
 
         return emails_df  # Return the DataFrame
 
@@ -119,7 +146,9 @@ if __name__ == "__main__":
     json_dir = os.path.join(os.path.dirname(__file__), "../data/emails")
     data_wrangler = DataWrangler(json_dir)
 
+    save = f'{os.path.join(os.getcwd(), ".")}'
     emails_df = data_wrangler.parse_emails(
-        save_csv_path="data/emails.csv", save_db_path="data/emails.db"
+        save_csv_path=f"{save}/emails.csv",  # Save the DataFrame to a CSV file
+        save_db_path=f"{save}/emails.db",  # Save the DataFrame to SQLite database
     )
     print(emails_df.head())
